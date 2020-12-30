190ab047e840e4d088d779573b3b26ff17c84365
Tuwuh S <tuwuhs@gmail.com>
2020-12-30 12:53:42 -0500

    Load image using OpenCV (requires CMake)

--- minimal.cc
+++ minimal.cc
@@ -13,8 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include <cstdio>
+
+#include <opencv2/opencv.hpp>
+
 #include "edgetpu.h"
 #include "edgetpu_c.h"
+#include "tensorflow/lite/builtin_op_data.h"
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
@@ -36,6 +40,80 @@ limitations under the License.
     fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__); \
     exit(1);                                                 \
   }
+
+// From https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/label_image/bitmap_helpers_impl.h
+template <class T>
+void resize(T* out, uint8_t* in, int image_height, int image_width,
+            int image_channels, int wanted_height, int wanted_width,
+            int wanted_channels, TfLiteType input_type) {
+  int number_of_pixels = image_height * image_width * image_channels;
+  std::unique_ptr<tflite::Interpreter> interpreter(new tflite::Interpreter);
+
+  int base_index = 0;
+
+  // two inputs: input and new_sizes
+  interpreter->AddTensors(2, &base_index);
+  // one output
+  interpreter->AddTensors(1, &base_index);
+  // set input and output tensors
+  interpreter->SetInputs({0, 1});
+  interpreter->SetOutputs({2});
+
+  // set parameters of tensors
+  TfLiteQuantizationParams quant;
+  interpreter->SetTensorParametersReadWrite(
+      0, kTfLiteFloat32, "input",
+      {1, image_height, image_width, image_channels}, quant);
+  interpreter->SetTensorParametersReadWrite(1, kTfLiteInt32, "new_size", {2},
+                                            quant);
+  interpreter->SetTensorParametersReadWrite(
+      2, kTfLiteFloat32, "output",
+      {1, wanted_height, wanted_width, wanted_channels}, quant);
+
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  const TfLiteRegistration* resize_op =
+      resolver.FindOp(tflite::BuiltinOperator_RESIZE_BILINEAR, 1);
+  auto* params = reinterpret_cast<TfLiteResizeBilinearParams*>(
+      malloc(sizeof(TfLiteResizeBilinearParams)));
+  params->align_corners = false;
+  params->half_pixel_centers = false;
+  interpreter->AddNodeWithParameters({0, 1}, {2}, nullptr, 0, params, resize_op,
+                                     nullptr);
+
+  interpreter->AllocateTensors();
+
+  // fill input image
+  // in[] are integers, cannot do memcpy() directly
+  auto input = interpreter->typed_tensor<float>(0);
+  for (int i = 0; i < number_of_pixels; i++) {
+    input[i] = in[i];
+  }
+
+  // fill new_sizes
+  interpreter->typed_tensor<int>(1)[0] = wanted_height;
+  interpreter->typed_tensor<int>(1)[1] = wanted_width;
+
+  interpreter->Invoke();
+
+  auto output = interpreter->typed_tensor<float>(2);
+  auto output_number_of_pixels = wanted_height * wanted_width * wanted_channels;
+
+  for (int i = 0; i < output_number_of_pixels; i++) {
+    switch (input_type) {
+      case kTfLiteFloat32:
+        out[i] = output[i] / 255.0;
+        break;
+      case kTfLiteInt8:
+        out[i] = static_cast<int8_t>(output[i] - 128);
+        break;
+      case kTfLiteUInt8:
+        out[i] = static_cast<uint8_t>(output[i]);
+        break;
+      default:
+        break;
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
   // Query Edge TPUs
@@ -69,11 +147,12 @@ int main(int argc, char* argv[]) {
   }
 
 
-  if (argc != 2) {
-    fprintf(stderr, "minimal <tflite model>\n");
+  if (argc != 3) {
+    fprintf(stderr, "minimal <tflite model> <image file>\n");
     return 1;
   }
   const char* filename = argv[1];
+  const char* image_filename = argv[2];
 
   // Load model
   std::unique_ptr<tflite::FlatBufferModel> model =
@@ -103,23 +182,110 @@ int main(int argc, char* argv[]) {
   // This will fail when an Edge TPU model is used but no Edge TPU devices
   // connected.
   TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
-  printf("=== Pre-invoke Interpreter State ===\n");
-  tflite::PrintInterpreterState(interpreter.get());
+  // printf("=== Pre-invoke Interpreter State ===\n");
+  // tflite::PrintInterpreterState(interpreter.get());
 
   // Fill input buffers
   // TODO(user): Insert code to fill input tensors.
   // Note: The buffer of the input tensor with index `i` of type T can
   // be accessed with `T* input = interpreter->typed_input_tensor<T>(i);`
+  // float* input = interpreter->typed_input_tensor<float>(0);
+  const std::vector<int> inputs = interpreter->inputs();
+  const std::vector<int> outputs = interpreter->outputs();
+
+  printf("Inputs: ");
+  for (auto i: inputs) {
+    printf("%d ", i);
+  }
+  printf("\n");
+
+  printf("Outputs: ");
+  for (auto o: outputs) {
+    printf("%d ", o);
+  }
+  printf("\n");
+
+  int input = inputs[0];
+  TfLiteType input_type = interpreter->tensor(input)->type;
+  TfLiteIntArray* input_dims = interpreter->tensor(input)->dims;
+  int wanted_height = input_dims->data[1];
+  int wanted_width = input_dims->data[2];
+  int wanted_channels = input_dims->data[3];
+  printf("Input dims: %d %d %d\n", wanted_height, wanted_width, wanted_channels);
+
+  cv::Mat image = cv::imread(argv[2], cv::IMREAD_COLOR);
+  // printf("Image dims: %d %d %d\n", image.rows, image.cols, image.channels());
+  cv::Mat image_rgb;
+  cv::cvtColor(image, image_rgb, cv::COLOR_BGR2RGB);
+  int image_height = image_rgb.rows;
+  int image_width = image_rgb.cols;
+  int image_channels = image_rgb.channels();
+  printf("Image dims: %d %d %d\n", image_height, image_width, image_channels);
+
+  switch (input_type) {
+    case kTfLiteFloat32:
+      resize<float>(interpreter->typed_tensor<float>(input), image_rgb.data,
+                    image_height, image_width, image_channels, wanted_height,
+                    wanted_width, wanted_channels, input_type);
+      break;
+    case kTfLiteInt8:
+      resize<int8_t>(interpreter->typed_tensor<int8_t>(input), image_rgb.data,
+                     image_height, image_width, image_channels, wanted_height,
+                     wanted_width, wanted_channels, input_type);
+      break;
+    case kTfLiteUInt8:
+      resize<uint8_t>(interpreter->typed_tensor<uint8_t>(input), image_rgb.data,
+                      image_height, image_width, image_channels, wanted_height,
+                      wanted_width, wanted_channels, input_type);
+      break;
+    // default:
+    //   LOG(ERROR) << "cannot handle input type "
+    //              << interpreter->tensor(input)->type << " yet";
+    //   exit(-1);
+  }
 
   // Run inference
   TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-  printf("\n\n=== Post-invoke Interpreter State ===\n");
-  tflite::PrintInterpreterState(interpreter.get());
+  // printf("\n\n=== Post-invoke Interpreter State ===\n");
+  // tflite::PrintInterpreterState(interpreter.get());
 
   // Read output buffers
   // TODO(user): Insert getting data out code.
   // Note: The buffer of the output tensor with index `i` of type T can
   // be accessed with `T* output = interpreter->typed_output_tensor<T>(i);`
+  int output = outputs[0];
+  TfLiteType output_type = interpreter->tensor(output)->type;
+  TfLiteIntArray* output_dims = interpreter->tensor(output)->dims;
+  int output_size = output_dims->data[output_dims->size - 1];
+  printf("Output dims: ");
+  for (size_t i = 0; i < output_dims->size; ++i) {
+    printf("%d ", output_dims->data[i]);
+  }
+  printf("\n");
+
+  printf("Output: ");
+  switch (output_type) {
+    case kTfLiteFloat32:
+      for (size_t i = 0; i < output_size; ++i) {
+        printf("%f ", interpreter->typed_tensor<float>(output)[i]);
+      }
+      break;
+    case kTfLiteInt8:
+      for (size_t i = 0; i < output_size; ++i) {
+        printf("%d ", interpreter->typed_tensor<int8_t>(output)[i]);
+      }
+      break;
+    case kTfLiteUInt8:
+      for (size_t i = 0; i < output_size; ++i) {
+        printf("%d ", interpreter->typed_tensor<uint8_t>(output)[i]);
+      }
+      break;
+    // default:
+    //   LOG(ERROR) << "cannot handle output type "
+    //              << interpreter->tensor(output)->type << " yet";
+    //   exit(-1);
+  }
+  printf("\n");
 
   interpreter.reset();
   edgetpu_context.reset();
